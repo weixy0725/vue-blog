9958e28f43cdfc67a44307d4b7bf3f230f339e3c
weixy0725 <weixy0725@sina.com>
2019-04-22 18:45:30 +0800

    提交细节

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,7 +1,23 @@
 <template>
+    <div>
+    <div class="article-list">
     <el-row class="row-size">
-        <ElCol :span="24">测试</ElCol>
+        <ElCol :span="24">
+            <div class="item">
+                    <a class="header" href="https://my.oschina.net/u/2478308/blog/1580589" target="_blank">
+                        <div class="ui teal label horizontal" data-tooltip="原创">原</div>
+                        gRPC Java 应用教程
+                    </a>
+
+            </div>
+        </ElCol>
     </el-row>
+   </div>
+    <el-row class="pagination-class">
+        <el-pagination background layout="prev, pager, next" :total="1000" class="test">
+        </el-pagination>
+    </el-row>
+    </div>
 </template>
 
 <style scoped>
@@ -15,4 +31,31 @@
         border-radius: 4px;
         line-height: 30px;
     }
+
+    .pagination-class{
+      margin-top: 30px;
+    }
+
+    .article-list .header .horizontal.label {
+    vertical-align: middle!important;
+    margin-top: -2px;
+    padding: .4em;
+    min-width: 0;
+    margin-right: 0;
+    }
+
+    .ui.teal.label, .ui.teal.labels .label {
+    background-color: #00b5ad!important;
+    border-color: #00b5ad!important;
+    color: #fff!important;
+   } 
+
+   
+</style>
+
+<style>
+    .el-pagination.is-background .el-pager li:not(.disabled).active{
+    background-color: cadetblue;
+    color: #FFF;
+    }  
 </style>
