87cc5850426aed8586c956340a79f216dc67854c
weixy0725 <weixy0725@sina.com>
2019-05-06 18:51:31 +0800

    提交侧边栏修改

--- src/components/common/SideBar.vue
+++ src/components/common/SideBar.vue
@@ -1,61 +1,47 @@
 <template>
-    <el-row class="tac test">
-        <el-col :span="24">
-            <el-menu default-active="" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose">
-                <el-submenu index="1">
-                    <template slot="title">
-                        <!-- <i class="el-icon-location"></i> -->
-                        <span>分类1</span>
-                    </template>
-                    <el-menu-item-group>
-                        <template slot="title">分组一</template>
-                        <el-menu-item index="1-1">选项1</el-menu-item>
-                        <el-menu-item index="1-2">选项2</el-menu-item>
-                    </el-menu-item-group>
-                    <el-menu-item-group title="分组2">
-                        <el-menu-item index="1-3">选项3</el-menu-item>
-                    </el-menu-item-group>
-                    <el-submenu index="1-4">
-                        <template slot="title">选项4</template>
-                        <el-menu-item index="1-4-1">选项1</el-menu-item>
-                    </el-submenu>
-                </el-submenu>
-                <el-menu-item index="2" @click="test">
-                    <span slot="title">导航二</span>
-                </el-menu-item>
-                <el-menu-item index="3" disabled>
-                    <span slot="title">导航三</span>
-                </el-menu-item>
-                <el-menu-item index="4">
-                    <span slot="title">导航四</span>
-                </el-menu-item>
-            </el-menu>
-        </el-col>
-    </el-row>
+  <el-row class="tac test">
+    <el-col :span="24">
+      <el-menu default-active class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose">
+        <el-menu-item index="1" @click="test">
+          <span slot="title">导航一</span>
+        </el-menu-item>
+        <el-menu-item index="2" @click="test">
+          <span slot="title">导航二</span>
+        </el-menu-item>
+        <el-menu-item index="3">
+          <span slot="title">导航三1213</span>
+        </el-menu-item>
+        <el-menu-item index="4">
+          <span slot="title">导航四</span>
+        </el-menu-item>
+      </el-menu>
+    </el-col>
+  </el-row>
 </template>
 
 
 <script>
-    export default {
-        methods: {
-            handleOpen(key, keyPath) {
-                console.log(key, keyPath);
-            },
-            handleClose(key, keyPath) {
-                console.log(key, keyPath);
-            },
-            test(key) {
-                console.log(key.index);
-            }
-        }
+export default {
+  methods: {
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    test(key) {
+      console.log(key.index);
     }
+  }
+};
 </script>
 
 <style scoped>
-    .test {
-        width: 200px;
-        margin-left: 10%;
-        margin-top: 30%;
-        pointer-events: auto;
-    }
+.test {
+  width: 200px;
+  margin-left: 10%;
+  margin-top: 30%;
+  pointer-events: auto;
+  text-align: right;
+}
 </style>
